3eb67bbcd1fc6bb6c09e760e317b9f83b6c86cb1
Jake Hildreth <jakehildreth@trimarcsecurity.com>
2023-08-09 05:58:28 -0400

    Added tagline lol

--- Unfuck-AD.ps1
+++ Unfuck-AD.ps1
@@ -4,3 +4,5 @@ for ($i = 1; $i -le $NumberOfAdObjects; $i++ ) {
     Write-Progress -Activity "Unfucking AD:" -Status "$i/$NumberOfAdObjects Complete." -PercentComplete $Percent
     Start-Sleep -Milliseconds ($i * 0.00000001 * [Math]::Pow($Percent,2))
 }
+
+Write-Output "If you've made it this far, you know securing Active Directory is hard. Trimarc can help: https://trimarcsecurity.com."
